9bfcc028b42ed028e81a446e481714bbe6823081
DevinFrenze <frenze.devin@gmail.com>
2016-08-05 19:17:45 -0700

    new chrome version updates the page every second. firefox build failed fixes issue #1

--- src/firefox/data/content-script.js
+++ src/firefox/data/content-script.js
@@ -1,18 +1,22 @@
 var elements = document.getElementsByTagName('*');
 
-for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
+function replaceWords() {
+  for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+      for (var j = 0; j < element.childNodes.length; j++) {
+          var node = element.childNodes[j];
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/Trump/g, 'Voldemort');
+          if (node.nodeType === 3) {
+              var text = node.nodeValue;
+              var replacedText = text.replace(/Trump/g, 'Voldemort');
 
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
-        }
-    }
+              if (replacedText !== text) {
+                  element.replaceChild(document.createTextNode(replacedText), node);
+              }
+          }
+      }
+  }
 }
+replaceWords()
+setInterval(replaceWords, 1000)
